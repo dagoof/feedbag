618d5c5753913e9d5f05c093e0f9705e4491ef7b
Peter DeGroff <peter@roamz.com>
2013-05-01 09:16:06 +1000

    race condition removed by moving all hash operations into select guarded method

--- broadcast.go
+++ broadcast.go
@@ -1,40 +1,59 @@
 package feedbag
 
 import (
+	"fmt"
 	"net/http"
-	"strings"
-	"io"
 )
 
 type Pool struct {
-	in     chan string
-	out    map[chan string]bool
-	forget chan chan string
+	in          chan string
+	out         map[chan string]bool
+	add, remove chan chan string
 }
 
-func NewPool(c chan string) Pool {
-	pool := Pool{c, map[chan string]bool{}, make(chan chan string)}
-	go pool.Broadcast()
-	return pool
+func NewPool(c chan string) *Pool {
+	pool := Pool{
+		c,
+		make(map[chan string]bool),
+		make(chan chan string),
+		make(chan chan string),
+	}
+	go pool.accept()
+	return &pool
 }
 
-func (p Pool) Broadcast() {
+// Act as a mutex, by only performing one operation on internal hash at a time
+// within this select
+func (p *Pool) accept() {
+	var cs chan string
+	var s string
+
 	for {
-		message := <-p.in
-		for target, _ := range p.out {
-			target <- message
+		select {
+		case cs = <-p.remove:
+			delete(p.out, cs)
+		case cs = <-p.add:
+			p.out[cs] = true
+		case s = <-p.in:
+			p.broadcast(s)
 		}
 	}
 }
 
-func (p Pool) Add() chan string {
+func (p *Pool) broadcast(s string) {
+	for target, _ := range p.out {
+		target <- s
+	}
+}
+
+func (p *Pool) Add() chan string {
 	c := make(chan string)
-	p.out[c] = true
+	p.add <- c
 	return c
 }
 
-func (p Pool) Forget(c chan string) {
-	delete(p.out, c)
+func (p *Pool) Forget(c chan string) {
+	p.remove <- c
 	close(c)
 }
 
@@ -44,13 +63,13 @@ func SharedStream(data chan string, preface ...string) http.HandlerFunc {
 		messages := pool.Add()
 		defer pool.Forget(messages)
 
-		//w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Content-Type", "text/event-stream")
 		for _, part := range preface {
-			io.Copy(w, strings.NewReader(part))
+			fmt.Fprintf(w, part)
 		}
 		if f, ok := w.(http.Flusher); ok {
 			for {
-				_, err := io.Copy(w, strings.NewReader(<-messages))
+				_, err := fmt.Fprintf(w, <-messages)
 				if err != nil {
 					return
 				}
